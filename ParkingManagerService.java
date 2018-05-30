2d478b609d391daf803a3f8d71bff0a19300a5d7
ohadwolski <ohadwolski@gmail.com>
2018-05-30 10:38:35 +0300

    Create ParkingManagerService.java

---
+++ ParkingManagerService.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parkingmanagerservice;
+
+/**
+ *
+ * @author Ohad Wolski
+ */
+public class ParkingManagerService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        initialize();
+        
+        
+        ConnectionThread ConnectionThread = new ConnectionThread( "ConnectionThread");
+        ConnectionThread.start();
+        
+        ListenerThread ListenerThread = new ListenerThread( "ConnectionThread");
+        ListenerThread.start();
+        
+        SenderThread SenderThread = new SenderThread( "ConnectionThread");
+        SenderThread.start();
+        
+        MsgQueuesHandler MsgQueuesHandler = new MsgQueuesHandler( "ConnectionThread");
+        MsgQueuesHandler.start();
+        
+        DataBaseUpdater DataBaseUpdater = new DataBaseUpdater( "ConnectionThread"); // both for DB and user interface
+        DataBaseUpdater.start();
+        
+    }
+    
+     /**
+     */
+    public static void initialize() {
+        // TODO code application logic here
+        // initialize all of the data structures?
+        // is this how it is made?
+        
+    }
+
+}
