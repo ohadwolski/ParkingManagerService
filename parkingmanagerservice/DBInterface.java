855d436beaf0a2c63deb65fa06a2477efe15daf4
hra46 <39758374+hra46@users.noreply.github.com>
2018-06-08 16:00:23 +0300

    Add files via upload

--- parkingmanagerservice/DBInterface.java
+++ parkingmanagerservice/DBInterface.java
@@ -2,7 +2,7 @@
 
 import java.util.Date;
 import java.util.Vector;
-
+import static java.lang.Math.toIntExact;
 
 
 enum SensorType {
@@ -46,24 +46,20 @@ public class DBInterface {
 		DB = new SystemDB();
 	}
 	
-	public class SystemDB {
-		public ParkingLotSign MainSign;
+	public static class SystemDB {
+		//public ParkingLotSign MainSign;
 		public ParkingLot ParkLot;
-		public AdminLogs LogsOfAdmin;
-		public ParkingLogs LogsOfParking;
-		
+		public Logs SystemLogs;
 		
 		private SystemDB( ) {
 			
 			Date date = new Date (0);             //need to check about Date
-			MainSign = new SystemDB.ParkingLotSign (0, SignArrow.NO_ENTRY, 0, SignStatus.ERROR, date);
-			ParkLot = new SystemDB.ParkingLot(0, IsOpen.FALSE, 0, SignArrow.NO_ENTRY, 0, SignStatus.ERROR, date, 0, 0);
-			LogsOfAdmin = new SystemDB.AdminLogs(0, 0, 0, date, AdminLogsEvent.ADMIN_LOGIN);
-			LogsOfParking = new SystemDB.ParkingLogs(0, date, ParkingLotLogsEvent.PARKING_FULL);
-			
+			//MainSign = new SystemDB.ParkingLotSign (0, SignArrow.NO_ENTRY, 0, SignStatus.ERROR, date);
+			ParkLot = new SystemDB.ParkingLot(0, IsOpen.FALSE, 0, SignArrow.NO_ENTRY, 0, SignStatus.ERROR, date, 0);
+			SystemLogs = new Logs();
 		}
 			
-		public class ParkingLotSign {                  //same sign for everyone
+		public static class ParkingLotSign {                  //same sign for everyone
 			
 			public int ID;
 			public SignArrow signArrow;
@@ -81,42 +77,46 @@ public class DBInterface {
 			}	
 		}	
 		
-		public class ParkingLot {
+		public static class ParkingLot {
 				
 			public int ParkID;
 			public IsOpen ParkingLotStatus;
 			public ParkingLotSign LotSign;
 			public int NumOfLevels;
-			public Vector<Levels> parkingLotLevels;
+			public int NumOfSpotsInLot;
 			public int NumOfFreeSpotsInLot;
+			public Vector<Levels> parkingLotLevels;
+			
 				
-			public ParkingLot(int parkId, IsOpen statusOfPark, int signId, SignArrow signArrow, int signCounter, SignStatus statusOfSign, Date signLastResponse,  int numOfLevels, int numOfParkingSpot) {
+			public ParkingLot(int parkId, IsOpen statusOfPark, int signId, SignArrow signArrow, int signCounter, SignStatus statusOfSign, Date signLastResponse, int numOfParkingSpot) {
 					
 				ParkID = parkId;
 				ParkingLotStatus = statusOfPark;
 				LotSign = new ParkingLotSign(signId, signArrow, signCounter, statusOfSign, signLastResponse);
-				NumOfLevels = numOfLevels;
-				parkingLotLevels = new Vector<Levels>(numOfLevels, 1); 
+				NumOfLevels = 0;
+				NumOfSpotsInLot = numOfParkingSpot;
 				NumOfFreeSpotsInLot = numOfParkingSpot;
+				parkingLotLevels = new Vector<Levels>(0, 1); 
 			}
 				
-				public class Levels{
+				public static class Levels{
 					
 					public int LevelID;
 					public IsOpen LevelStatus;
 					public ParkingLotSign LevelSign;
 					public int NumOfAreas;
-					public Vector<Areas> LevelsAreas;
 					public int NumOfFreeSpotsInLevel;
+					public Vector<Areas> LevelsAreas;
+					
 				
 					public Levels(int levelId, IsOpen statusOfLevel, int signId, SignArrow signArrow, int signCounter, SignStatus statusOfSign, Date signLastResponse,  int numOfAreas, int numOfParkingSpot) {
 						
 						LevelID = levelId;
 						LevelStatus = statusOfLevel;
 						LevelSign = new ParkingLotSign(signId, signArrow, signCounter, statusOfSign, signLastResponse);
-						NumOfAreas = numOfAreas;
-						LevelsAreas = new Vector<Areas>(numOfAreas, 1); 
+						NumOfAreas = 0;
 						NumOfFreeSpotsInLevel = numOfParkingSpot;
+						LevelsAreas = new Vector<Areas>(0, 1); 
 					}
 					
 					public class Areas {
@@ -154,39 +154,49 @@ public class DBInterface {
 						}		
 					}		
 				}						
-			}	
-		public class AdminLogs {
-				
-			public int AdminID;
-			public int SignID;
-			public int ParkingSpotID;
-			public Date Date;
-			public AdminLogsEvent LogEvent;
-				
-			public AdminLogs(int adminId, int signId, int spotId, Date date, AdminLogsEvent logEvent) {
-					
-				AdminID = adminId;
-				SignID = signId;
-				ParkingSpotID = spotId;
-				Date = date;
-				LogEvent = logEvent;
 			}
-		}
+		public static class Logs {
 			
-		public class ParkingLogs {
-				
-			public int ParkSpotSensorID;
-			public Date Date;
-			public ParkingLotLogsEvent ParkingLotLogsEvent;
-				
-			public ParkingLogs (int sensorId, Date date, ParkingLotLogsEvent logEvent) {
-					
-				ParkSpotSensorID = sensorId;
-				Date = date;
-				ParkingLotLogsEvent = logEvent;
+			public Vector<Logs.AdminLogs> LogsOfAdmin;
+			public Vector<Logs.ParkingLogs> LogsOfParking;
+			
+			public Logs() {
+				LogsOfAdmin = new Vector<Logs.AdminLogs>(0,1);
+				LogsOfParking = new Vector<Logs.ParkingLogs>(0,1);
 			}
+			
+			public static class AdminLogs {
 				
-		}	
+				public int AdminID;
+				public int SignID;
+				public int ParkingSpotID;
+				public Date Date;
+				public AdminLogsEvent LogEvent;
+				
+				public AdminLogs(int adminId, int signId, int spotId, Date date, AdminLogsEvent logEvent) {
+					
+					AdminID = adminId;
+					SignID = signId;
+					ParkingSpotID = spotId;
+					Date = date;
+					LogEvent = logEvent;
+				}
+			}
+			
+			public static class ParkingLogs {
+				
+				public int ParkSpotSensorID;
+				public Date Date;
+				public ParkingLotLogsEvent ParkingLotLogsEvent;
+				
+				public ParkingLogs (int sensorId, Date date, ParkingLotLogsEvent logEvent) {
+					
+					ParkSpotSensorID = sensorId;
+					Date = date;
+					ParkingLotLogsEvent = logEvent;
+				}	
+			}
+	}	
 	
 	
 		
@@ -199,89 +209,158 @@ public class DBInterface {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	
+
 	
+/////////////////////////////////////////////Parking Lot////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-/////////////////////////////////////////////////Parkinglot's main sign methods///////////////////////////////////////////////////////
 	
 	public Results EditParkingLotSign(int id, SignArrow arrow, int counter, SignStatus status, Date lastResponse) {
 		
-		DB.MainSign.ID = id;
-		DB.MainSign.signArrow = arrow;
-		DB.MainSign.signCounter = counter;
-		DB.MainSign.signStatus = status;
-		DB.MainSign.LastResponse = lastResponse;
+		DB.ParkLot.LotSign.ID = id;
+		DB.ParkLot.LotSign.signArrow = arrow;
+		DB.ParkLot.LotSign.signCounter = counter;
+		DB.ParkLot.LotSign.signStatus = status;
+		DB.ParkLot.LotSign.LastResponse = lastResponse;
 		return Results.SUCCESS;
 	}
 	
 	
-	public void setMainSignID(int id) {
+//////////////////////////Parking Lot ID methods///////////////////////////////////////
+	
+	public void setParkLotID(int id) {
 		
-		DB.MainSign.ID = id;
+		DB.ParkLot.ParkID = id;
 	}
 	
-	public int getMainSignID() {
+	public int getParkLotID() {
 		
-		return DB.MainSign.ID;
-	}
-	
-	public void setMainSignArrow(SignArrow arrow) {
-		
-		DB.MainSign.signArrow = arrow;
-	}
-	
-	public SignArrow getMainSignArrow() {
-		
-		return DB.MainSign.signArrow;
-	}
-	
-	public void setMainSignCounter(int counter) {
-		
-		DB.MainSign.signCounter = counter;
-	}
-	
-	public int getMainSignCounter() {
-		
-		return DB.MainSign.signCounter;
-	}
-	
-	public void setMainSignStatus(SignStatus status) {
-		
-		DB.MainSign.signStatus = status;
+		return DB.ParkLot.ParkID;
 	}
 
-	public SignStatus getMainSignStatus() {
-	
-		return DB.MainSign.signStatus;
+//////////////////////////////Parking Lot Status////////////////////////////////////	
+	public void setParkLotStatus(IsOpen status) {
+		
+		DB.ParkLot.ParkingLotStatus = status;
 	}
 	
-	public void setMainSignLastResponse(Date lastResponse) {
+	public IsOpen getParkLotStatus() {
 		
-		DB.MainSign.LastResponse = lastResponse;	
+		return DB.ParkLot.ParkingLotStatus;
+	}
+
+//////////////////////////////////////Parking Lot num of levels///////////////////
+	public void setParkLotNumOfLevels(int num) {
+		
+		DB.ParkLot.NumOfLevels = num;
 	}
 	
-	public Date LastResponse() {
+	public int getParkLotNumOfLevels() {
 		
-		return DB.MainSign.LastResponse;
+		return DB.ParkLot.NumOfLevels;
+	}
+	
+////////////////////////////////////Parking Lot num of spots//////////////////////
+	
+	public void setParkLotNumOfSpots(int num) {
+		
+		DB.ParkLot.NumOfSpotsInLot = num;
+	}
+	
+	public int getParkLotNumOfSpots() {
+		
+		return DB.ParkLot.NumOfSpotsInLot;
+	}	
+	
+	public void setParkLotNumOfFreeSpots(int num) {
+		
+		DB.ParkLot.NumOfFreeSpotsInLot = num;
+	}
+	
+	public int getParkLotNumOfFreeSpots() {
+		
+		return DB.ParkLot.NumOfFreeSpotsInLot;
+	}	
+	
+	public void incParkLotNumOfFreeSpots(int num) {
+		
+		DB.ParkLot.NumOfFreeSpotsInLot++;
+	}
+	
+	public void decParkLotNumOfFreeSpots() {
+		
+		DB.ParkLot.NumOfFreeSpotsInLot--;
+	}	
+
+/////////////////////////////////////////////////Parkinglot's main sign methods///////////////////////////////////////////////////////
+	
+	public void setParkLotSignID(int id) {
+		
+		DB.ParkLot.LotSign.ID = id;
+	}
+	
+	public int getParkLotSignID() {
+		
+		return DB.ParkLot.LotSign.ID;
+	}
+	
+	
+	public void setParkLotSignArrow(SignArrow arrow) {
+		
+		DB.ParkLot.LotSign.signArrow = arrow;
+	}
+	
+	public SignArrow getParkLotSignArrow() {
+		
+		return DB.ParkLot.LotSign.signArrow;
+	}
+	
+	public void setParkLotSignCounter(int counter) {
+		
+		DB.ParkLot.LotSign.signCounter = counter;
+	}
+	
+	public int getParkLotSignCounter() {
+		
+		return DB.ParkLot.LotSign.signCounter;
+	}
+	
+	public void setParkLotSignStatus(SignStatus status) {
+		
+		DB.ParkLot.LotSign.signStatus = status;
+	}
+
+	public SignStatus getParkLotSignStatus() {
+	
+		return DB.ParkLot.LotSign.signStatus;
+	}
+	
+	public void setParkLotSignLastResponse(Date lastResponse) {
+		
+		DB.ParkLot.LotSign.LastResponse = lastResponse;	
+	}
+	
+	public Date getParkLotSignLastResponse() {
+		
+		return DB.ParkLot.LotSign.LastResponse;
 	}
 	
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////Parkinglot's Level methods////////////////////////////////////////////////////////////////
+	
+	public Results AddNewLevelToParkingLot(int levelId, IsOpen statusOfLevel, int signId, SignArrow signArrow, int signCounter, SignStatus statusOfSign, Date signLastResponse,  int numOfAreas, int numOfParkingSpot) {
+		
+		
+		SystemDB.ParkingLot.Levels LevelToAdd = new SystemDB.ParkingLot.Levels(levelId, statusOfLevel, signId, signArrow, signCounter, statusOfSign, signLastResponse, numOfAreas, numOfParkingSpot);
+		DB.ParkLot.parkingLotLevels.insertElementAt(LevelToAdd, levelId);
+		DB.ParkLot.NumOfLevels++;
+		
+		return Results.SUCCESS;
+	}	
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+	
+	
 	
 	
 ///////////////////////////////////////////////////Admin's logs methods///////////////////////////////////////////////////
@@ -289,14 +368,21 @@ public class DBInterface {
 	
 	public Results AddNewAdminLog(int adminId, int signId, int spotId, Date date, AdminLogsEvent logEvent) {
 		
-		DB.LogsOfAdmin.AdminID = adminId;
-		DB.LogsOfAdmin.SignID = signId;
-		DB.LogsOfAdmin.ParkingSpotID = spotId;
-		DB.LogsOfAdmin.Date = date;
-		DB.LogsOfAdmin.LogEvent = logEvent;
+		
+		SystemDB.Logs.AdminLogs LogToAdd = new SystemDB.Logs.AdminLogs(adminId, signId, spotId, date, logEvent);
+		int index = Math.toIntExact(date.getTime());  										 //long to int conversion
+		DB.SystemLogs.LogsOfAdmin.insertElementAt(LogToAdd, index);
 		return Results.SUCCESS;
 	}
 	
+	public void printAdminLogs() {
+		
+		for (int i=1; i<DB.SystemLogs.LogsOfAdmin.capacity()+1; i++) {
+			
+			System.out.println("Admin Id is" + DB.SystemLogs.LogsOfAdmin.get(i).AdminID + ", The sign Id is" + DB.SystemLogs.LogsOfAdmin.get(i).SignID + ", The spot Id is" + DB.SystemLogs.LogsOfAdmin.get(i).ParkingSpotID + ",Time is" + DB.SystemLogs.LogsOfAdmin.get(i).Date + ",The Event is" + DB.SystemLogs.LogsOfAdmin.get(i).LogEvent);
+		}
+	}
+	/*
 	public void setLogsOfAdminAdminID(int adminId) {
 		
 		DB.LogsOfAdmin.AdminID = adminId;
@@ -346,20 +432,28 @@ public class DBInterface {
 		
 		return DB.LogsOfAdmin.LogEvent;
 	}
-	
+	*/
 	
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 ///////////////////////////////////////////////////Parking logs methods///////////////////////////////////////////////////	
 	
-public Results AddNewParkingLog(int sensorId, Date date, ParkingLotLogsEvent logEvent) {
+	public Results AddNewParkingLog(int sensorId, Date date, ParkingLotLogsEvent logEvent) {
 		
-		DB.LogsOfParking.ParkSpotSensorID = sensorId;
-		DB.LogsOfParking.Date = date;
-		DB.LogsOfParking.ParkingLotLogsEvent = logEvent;
+		SystemDB.Logs.ParkingLogs LogToAdd = new SystemDB.Logs.ParkingLogs(sensorId, date, logEvent);
+		int index = Math.toIntExact(date.getTime());  										 //long to int conversion
+		DB.SystemLogs.LogsOfParking.insertElementAt(LogToAdd, index);
 		return Results.SUCCESS;
 	}
 	
+	public void printParkingLogs() {
+		
+		for (int i=1; i<DB.SystemLogs.LogsOfParking.capacity()+1; i++) {
+			
+			System.out.println("Sensor Id is" + DB.SystemLogs.LogsOfParking.get(i).ParkSpotSensorID + ",Time is" + DB.SystemLogs.LogsOfParking.get(i).Date + ",The Event is" + DB.SystemLogs.LogsOfParking.get(i).ParkingLotLogsEvent);
+		}
+	}
+	/*
 	public void setLogsOfParkingParkSpotSensorID(int sensorId) {
 		
 		DB.LogsOfParking.ParkSpotSensorID = sensorId;
@@ -388,10 +482,32 @@ public Results AddNewParkingLog(int sensorId, Date date, ParkingLotLogsEvent log
 	public ParkingLotLogsEvent getLogsOfParkingLogsEvent() {
 		
 		return DB.LogsOfParking.ParkingLotLogsEvent;
-	}
+	}*/
 	
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
-				
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }	
 		
 		
