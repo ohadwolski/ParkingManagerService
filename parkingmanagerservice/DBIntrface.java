ee258c86fd9dba72e9aeffeed9bf918629662172
hra46 <39758374+hra46@users.noreply.github.com>
2018-06-12 20:10:59 +0300

    Add files via upload

--- parkingmanagerservice/DBIntrface.java
+++ parkingmanagerservice/DBIntrface.java
@@ -1,5 +1,6 @@
 
 import java.util.Date;
+import java.util.Vector;
 //import java.util.Vector;
 
 public class DBIntrface {
@@ -12,10 +13,9 @@ public class DBIntrface {
 		
 	}
 	
-	
-	void setGeneralType(int id, Type symbol) {    //same as parktype
+	void setGeneralType(int id, Type type) {    //same as parktype
 		
-		DB.DBConfiguration.elementAt(id).setGeneralType(symbol);		
+		(findAnObject(DB.DBConfiguration, id)).setGeneralType(type);		
 	}
 	/*
 	void setParkType(int id, Type type) {
@@ -23,44 +23,212 @@ public class DBIntrface {
 		DB.DBConfiguration.elementAt(id).setGeneralType(type);		
 	}*/
 	
+	void addObject(DB.ConfigObj obj, int containingElementId) {
+		
+		int index = findIndex((findAnObject(DB.DBConfiguration, containingElementId)).Areas,obj.getID());
+		(findAnObject(DB.DBConfiguration, containingElementId)).Areas.insertElementAt(obj, index); 
+	}
+	
+	void removeObject(DB.ConfigObj obj, int containingElementId) {
+	
+		removeStatusElementFromDBStatusAfterDeleteOfContaining((findAnObject(DB.DBConfiguration, containingElementId)));
+		(findAnObject(DB.DBConfiguration, containingElementId)).Areas.removeElementAt(obj.getID()); 
+	}
+
 	void addSign(int signId, Type type, int containingElementId) { //same as add spot
 		
 		DB.Sign newSign = new DB.Sign(signId, type);
-		((DB.Area) DB.DBConfiguration.get(containingElementId)).Areas.insertElementAt(newSign, signId); 
+		int index = findIndex((findAnObject(DB.DBConfiguration, containingElementId)).Areas,newSign.getID());
+		(findAnObject(DB.DBConfiguration, containingElementId)).Areas.insertElementAt(newSign, index); 
 	}
 	
 	void removeSign(int signId, int containingElementId) {  //same as remove spot
 		
-		((DB.Area) DB.DBConfiguration.get(containingElementId)).Areas.removeElementAt(signId);
+		(findAnObject(DB.DBConfiguration, containingElementId)).Areas.removeElementAt(signId);
 	}
 	
 	void addParkingSpot(int spotId, Type type, int containingElementId) {
 		
 		DB.Spot newSpot = new DB.Spot(spotId, type);
-		((DB.Area) DB.DBConfiguration.get(containingElementId)).Areas.insertElementAt(newSpot, spotId);
+		int index = findIndex((findAnObject(DB.DBConfiguration, containingElementId)).Areas,newSpot.getID());
+		(findAnObject(DB.DBConfiguration, containingElementId)).Areas.insertElementAt(newSpot, index);
 	}
 	
 	void removeSpot(int spotId, int containingElementId) {
 		
-		((DB.Area) DB.DBConfiguration.get(containingElementId)).Areas.removeElementAt(spotId);
+		(findAnObject(DB.DBConfiguration, containingElementId)).Areas.removeElementAt(spotId);
 	}
 	
 	void addArea (int areaId, int containingElementId) {
 		
 		DB.Area newArea = new DB.Area(areaId, Type.OPEN);
-		((DB.Area) DB.DBConfiguration.get(containingElementId)).Areas.insertElementAt(newArea, areaId);
+		int index = findIndex((findAnObject(DB.DBConfiguration, containingElementId)).Areas,newArea.getID());
+		(findAnObject(DB.DBConfiguration, containingElementId)).Areas.insertElementAt(newArea, index);
 	}
 	
 	void removeArea(int areaId, int containingElementId) {
 		
-		((DB.Area) DB.DBConfiguration.get(containingElementId)).Areas.removeElementAt(areaId);
+		(findAnObject(DB.DBConfiguration, containingElementId)).Areas.removeElementAt(areaId);
 	}
+	
+	int whatKindOfObject(DB.ConfigObj obj) {
+		
+		if(obj instanceof DB.Area) {
+			
+			return 1;
+		}
+		else if(obj instanceof DB.Sign) {
+			
+			return 2;
+		}
+		else if(obj instanceof DB.Spot) {
+			
+			return 3;
+		}
+		return 0;
+		
+	}
+	
+	int findIndex(Vector<DB.ConfigObj> v,int id) {
+		
+		int index = 0;
+		for (DB.ConfigObj item : v) {
+			if (item.getID()>id) {
+				index = v.indexOf(item);
+				break;
+			}
+		}
+		return index;
+	}
+	
+	
+	
+	DB.ConfigObj findAnObject(Vector<DB.ConfigObj> v, int id ){
+		
+		
+		for (DB.ConfigObj item: v) {
+			
+			if (item.getID()==id) {
+				
+				return item;
+			}
+			else if(!item.Areas.isEmpty()) {
+				
+				return findAnObject(item.Areas, id);
+			}
+			
+		}
+		
+		return null;
+	}
+		
+	
 	
 	
 	//DB Status 
 	
+	int findIndexToInsertInDBStatus(int id) {
+			
+		int index = 0;
+		for (DB.StatusElementObj item : DB.DBStatus) {
+			if (item.getID()>id) {
+				index = DB.DBStatus.indexOf(item);
+				break;
+			}
+		}
+		return index;
+	}
 	
-	void addSignToDBStatus(int signId, Type Status, Date date) { 
+	int findTheIndexOfID(int id) {
+		
+		for (DB.StatusElementObj item : DB.DBStatus) {
+			if (item.getID() == id) {
+				return DB.DBStatus.indexOf(item);
+			}
+		}
+		return -1;
+	}
+	
+	void addStatusElementToDBStatus(DB.StatusElementObj obj ) {
+		
+		int index = findIndexToInsertInDBStatus(obj.getID());
+		DB.DBStatus.insertElementAt(obj, index);
+	}
+	
+	
+	void removeStatusElementFromDBStatus(int id ) {
+
+		DB.DBStatus.remove(findTheIndexOfID(id));
+			
+	}	
+	
+	void removeStatusElementFromDBStatusAfterDeleteOfContaining(DB.ConfigObj obj) {
+		
+		for (DB.ConfigObj item: obj.Areas) {
+			
+			if ((item instanceof DB.Sign) || (item instanceof DB.Spot) ) {
+				
+				removeStatusElementFromDBStatus(item.getID());
+			}
+			else if(!item.Areas.isEmpty()) {
+				
+				removeStatusElementFromDBStatusAfterDeleteOfContaining(item);
+			}
+		}	
+	
+	}
+	
+	void editStatusElementID(int oldID,int newID) {
+		
+		DB.DBStatus.get(findTheIndexOfID(oldID)).setID(newID);
+	}
+	
+	void setStatusElementLastResponse(int id, Date lastResponse) {
+		
+		DB.DBStatus.get(findTheIndexOfID(id)).setLastResponse(lastResponse);
+	}
+	
+	Date getStatusElementLastResponse(int id) {
+		
+		return DB.DBStatus.get(findTheIndexOfID(id)).getLastResponse();
+	}
+	
+	void setStatusElement(int id, Type status) {
+			
+		DB.DBStatus.get(findTheIndexOfID(id)).setStatus(status);;
+	}
+	 
+	Type getStatusElement(int id) {
+			
+		return DB.DBStatus.get(findTheIndexOfID(id)).getStatus();
+	}
+	
+	void setNewCounter(int id, int newCounter) {
+		
+		DB.DBStatus.get(findTheIndexOfID(id)).setCounter(newCounter);
+	}
+	
+	void changeCounter(int id, int flag) {
+		
+		if (flag == 0) {
+			
+			DB.DBStatus.get(findTheIndexOfID(id)).decCounter();
+		}
+		else {
+			
+			DB.DBStatus.get(findTheIndexOfID(id)).incCounter();
+		}
+	}
+	
+	
+	 
+		
+	
+	
+	
+	
+	/*
+	void addSignToDBStatus(int signId, Type Status, Date date) {            //same as spot?
 		
 		DB.SignStatus newSign = new DB.SignStatus(signId, Status, date);
 		DB.DBStatus.insertElementAt(newSign, signId);
@@ -77,7 +245,16 @@ public class DBIntrface {
 		DB.DBStatus.remove(Id);
 	}
 	
-	void setParkFree(int id) {
+	
+	void setStatus(int id, Type status) {
+		
+		
+		
+	}
+	
+	
+	/*
+	void setParkFree(int id) {          //same as taken
 		
 		DB.DBStatus.get(id).Status = Type.FREE;
 	}
@@ -87,11 +264,12 @@ public class DBIntrface {
 		DB.DBStatus.get(id).Status = Type.TAKEN;
 	}
 	
-	void setSignOKStatus(int id) {
+	void setSignOKStatus(int id) {      //same as error
 		
 		DB.DBStatus.get(id).Status = Type.OK;
 	}
-	
+	*/
+	/*
 	void setSignErrorStatus(int id) {
 		
 		DB.DBStatus.get(id).Status = Type.ERROR;
@@ -111,6 +289,6 @@ public class DBIntrface {
 		
 		return DB.DBStatus.get(id).LastResponse;
 	}
-	
+	*/
 
 }
